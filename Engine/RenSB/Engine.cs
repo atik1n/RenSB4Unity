c51328a6a10674d8dde6b70d9ced81e2997082e9
atikin <chizhovnikita2000@gmail.com>
2016-01-06 00:47:28 +0300

    Initial commit

---
+++ Engine/RenSB/Engine.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace RenSB
+{
+    public struct SpriteData
+    {
+        public Sprite Sprite;
+        public Vector2 Coord;
+        public string Name;
+
+        public SpriteData(Sprite SpritePath, Vector2 xy, string SpriteName)
+        {
+            Sprite = SpritePath;
+            Coord = xy;
+            Name = SpriteName;
+        }
+    }
+
+    public class Preferences
+    {
+        public static string ProjectPath;
+    }
+    public class Engine : MonoBehaviour
+    {
+        public static GameObject SpritesObj = new GameObject("Sprites");
+
+        public static void Init()
+        {
+            GameObject Main = new GameObject("RenSB");
+            Canvas Canvas = Main.AddComponent<Canvas>();
+            Canvas.renderMode = RenderMode.ScreenSpaceOverlay;
+            SpritesObj.transform.SetParent(Canvas.transform);
+            SpritesObj.AddComponent<RectTransform>();
+            SpritesObj.GetComponent<RectTransform>().localPosition = new Vector3(0, 0, 0);
+            SpritesObj.GetComponent<RectTransform>().sizeDelta = new Vector2(Screen.width, Screen.height);
+            Debug.Log(Screen.width + " " + Screen.height);
+        }
+
+        public class Sprites
+        {
+            static Image[] ImagesArr = new Image[0];
+            static GameObject[] ObjArr = new GameObject[0];
+
+            public static void InitDialog(string DialogData, params SpriteData[] spritesData)
+            {
+                for (int c = 0; c < spritesData.Length; c++)
+                {
+                    try
+                    {
+                        Debug.Log(spritesData[c].Sprite + " " + spritesData[c].Coord + " " + spritesData[c].Name);
+                        int W = spritesData[c].Sprite.texture.width;
+                        int H = spritesData[c].Sprite.texture.height;
+                        int Index = ImagesArr.Length;
+                        Array.Resize(ref ImagesArr, ImagesArr.Length + 1); Array.Resize(ref ObjArr, ObjArr.Length + 1);
+
+                        ObjArr[Index] = new GameObject();
+                        ObjArr[Index].transform.SetParent(SpritesObj.transform);
+
+                        ObjArr[Index].AddComponent<RectTransform>();
+                        ObjArr[Index].GetComponent<RectTransform>().localPosition = new Vector3(spritesData[c].Coord.x, spritesData[c].Coord.y, 0);
+                        ObjArr[Index].GetComponent<RectTransform>().sizeDelta = new Vector2(W, H);
+                        ObjArr[Index].name = spritesData[c].Name;
+
+                        ImagesArr[Index] = ObjArr[Index].AddComponent<Image>();
+                        ImagesArr[Index].name = spritesData[c].Name + "Sprite";
+                        ImagesArr[Index].sprite = spritesData[c].Sprite;
+                    }
+                    catch (Exception ex)
+                    {
+                        Debug.Log("ACHTUNG! " + ex);
+                    }
+                }
+            }
+        }
+
+        public static void BG(string Image, Image BG)
+        {
+
+        }
+    }
+}
---
+++ Engine/RenSB/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RenSB")]
+[assembly: AssemblyDescription("VN library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Atikin")]
+[assembly: AssemblyProduct("RenSB DLL")]
+[assembly: AssemblyCopyright("Copyright © Atikin Vozhich 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b7796f89-1bc5-434a-a1ed-c2474c519342")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
