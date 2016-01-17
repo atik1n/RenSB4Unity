9a20e2ba740d0dc00f4c7906a2229471264bf91f
atikin <chizhovnikita2000@gmail.com>
2016-01-17 19:25:54 +0300

    Убрал лишние комменты

--- Unity/Assets/CStest.cs
+++ Unity/Assets/CStest.cs
@@ -5,14 +5,12 @@ using RenSB;
 
 public class CStest : MonoBehaviour {
 
-	// Use this for initialization
 	void Start () {
         RenSB.Preferences.ProjectPath = "C:/Users/Никита/Dropbox/Приложения/smart BASIC/Katawa port/Katawa/Katawa Shoujo/";
         RenSB.Engine.Init();
         RenSB.Engine.Sprites.InitDialog("hi", new SpriteData(Resources.Load<Sprite>("Sprites/misha"), new Vector2(200, 0), "misha"));
 	}
 	
-	// Update is called once per frame
 	void Update () {
 	
 	}
